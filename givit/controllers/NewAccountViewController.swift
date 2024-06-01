5db70eebe7b3898fed85bca767cdafce6c2149fb
aiexxash <ialexballa@gmail.com>
2024-06-01 22:38:27 +0300

    feat: Account register
    Only storyboard interface

--- givit/controllers/NewAccountViewController.swift
+++ givit/controllers/NewAccountViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class NewAccountViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ givit/controllers/SignInViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignInViewController.swift
+//  givit
+//
+//  Created by Alex Balla on 01.06.2024.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationController?.navigationBar.tintColor = .systemRed
+    }
+    
+
+}
