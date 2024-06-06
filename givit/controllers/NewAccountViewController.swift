ed4fa760cb723901a27a4e8a29d09a3428dadbea
aiexxash <ialexballa@gmail.com>
2024-06-06 19:48:32 +0300

    feat: Check fields on fill

--- givit/controllers/NewAccountViewController.swift
+++ givit/controllers/NewAccountViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class NewAccountViewController: UIViewController {
-
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var passwordConfirmField: UITextField!
+    @IBOutlet weak var createButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func createButtonClicked(_ sender: UIButton) {
+        print("Clicked")
+        let error = checkFieldsOnFill(textFields: [emailField, passwordField, passwordConfirmField], errorLabel: errorLabel)
+        guard error == nil else {
+            return
+        }
+        errorLabel.textColor = .systemGreen
+        errorLabel.text = "Succeed"
+        errorLabel.isHidden = false
+    }
+}
 
-
+extension UIViewController {
+    func checkFieldsOnFill(textFields: [UITextField]?, errorLabel: UILabel) -> String? {
+    var errorText: String? = nil
+        
+        guard !textFields!.isEmpty else {
+            errorText = "No text fields set!"
+            errorLabel.isHidden = false
+            errorLabel.text = errorText!
+            return errorText!
+        }
+        
+        for field in textFields! {
+            if field.text == "" {
+                errorText = "You didn't fill all text fields properly!"
+                errorLabel.isHidden = false
+                errorLabel.text = errorText
+                field.layer.borderColor = UIColor.systemRed.cgColor
+                field.layer.borderWidth = 1
+                return errorText
+            }
+        }
+        
+        return nil
+    }
 }
 
--- givit/controllers/SignInViewController.swift
+++ givit/controllers/SignInViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 class SignInViewController: UIViewController {
-
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var signInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         navigationController?.navigationBar.tintColor = .systemRed
     }
     
